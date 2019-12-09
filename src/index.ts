5a149d3547450214b31911727b411526e6c36139
James Brumond <james@jbrumond.me>
2019-12-08 18:33:53 -0800

    export some types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { DatabasePool, ClusterConfig } from './pool';
 export { Query, QueryCompiler, QueryIsRetryableCallback, QueryParams, QueryResult, QueryConfig } from './query';
 export { SelectQuery, SelectQueryResult } from './select-query';
 export { WriteQuery, WriteQueryResult } from './write-query';
+export { HealthcheckResults, HealthcheckResult, TransactionType } from './mysql';
